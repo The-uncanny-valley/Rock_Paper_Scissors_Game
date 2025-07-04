67af5da7a0f895054c59d5abe7426b39a195f283
the-uncanny-valley <runcannyvalley@gmail.com>
2025-07-04 20:58:52 +0300

    Add user input function with input validation

--- src/Game.kt
+++ src/Game.kt
@@ -1,8 +1,28 @@
 fun main() {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
-     
+    val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice(optionsParam: Array<String>) =
     optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam : Array<String>) : String {
+    var isValidChoice = false
+    var userChoice = ""
+
+    while (!isValidChoice) {
+        println("Please enter one of the following:")
+        for (item in optionsParam) println(" $item")
+        println(".")
+
+        val userInput = readln()
+        if (userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+
+        if (!isValidChoice) println("You must enter a valid choice")
+    }
+    return userChoice
+}
